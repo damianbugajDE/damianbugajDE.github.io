227e06d687183f06e4b56c6a5ec34b4c41288fe2
Damian Bugaj <damianbugajit@gmail.com>
2026-01-25 20:56:43 +0100

    Create seo.js

---
+++ seo.js
@@ -0,0 +1,14 @@
+<Helmet>
+  <title>Energetyka Jądrowa: Czysta Energia Przyszłości</title>
+  <meta name="description" content="Kompleksowe kompendium wiedzy o energetyce jądrowej. Dowiedz się, jak działają reaktory SMR i dlaczego atom jest kluczem do dekarbonizacji." />
+  
+  {/* Open Graph / Facebook */}
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Energetyka Jądrowa - Nowoczesne Technologie" />
+  <meta property="og:description" content="Odkryj naukowe fakty o energii atomowej." />
+  <meta property="og:image" content="url_do_grafiki_podgladu.jpg" />
+
+  {/* Twitter */}
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Energetyka Jądrowa" />
+</Helmet>
